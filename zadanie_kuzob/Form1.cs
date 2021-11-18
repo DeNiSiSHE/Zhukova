6658356b36022da617cb91ab4365edc7c08c397e
Студенты Кузьминой О.Б <kuzob@teach.local>
2021-11-18 13:54:42 +0300

    Добавьте файлы проекта.

---
+++ zadanie_kuzob/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace zadanie_kuzob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String log_user = textBox1.Text;
+            String pass_user = textBox2.Text;
+
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `aoaoaa` WHERE `login` = @userLogin AND `password` = @userPass", db.getConnection());
+            command.Parameters.Add("@userLogin", MySqlDbType.VarChar).Value = log_user;
+            command.Parameters.Add("@userPass", MySqlDbType.VarChar).Value = pass_user;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (log_user == "" && pass_user == "")
+            {
+                MessageBox.Show("Значения пустые");
+            }
+            else if (log_user == "" || pass_user == "")
+            {
+                MessageBox.Show("Вы заполнили не все поля");
+            }
+            else if (table.Rows.Count > 0)
+            {
+                Hide();
+                Form3cs avtoriz = new Form3cs();
+                avtoriz.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Какая-та ошикбка");
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            {
+                Hide();
+                Regist reg = new Regist();
+                reg.ShowDialog();
+                this.Close();
+            }
+
+        }
+    }
+}
---
+++ zadanie_kuzob/Regist.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace zadanie_kuzob
+{
+    public partial class Regist : Form
+    {
+        public Regist()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlConnection DB = new MySqlConnection();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `aoaoaa` ( `login`, `password`, `Rul`,`FIO`) VALUES (@login, @pass, @rul, @fio)", db.getConnection());
+
+
+            command.Parameters.Add("@fio", MySqlDbType.VarChar).Value = textBox1.Text;
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = textBox2.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = textBox3.Text;
+            command.Parameters.Add("@rul", MySqlDbType.VarChar).Value = "NULL";
+
+            db.openConnection();
+            if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text == "")
+            {
+                MessageBox.Show("Введите данные для регистрации");
+                return;
+            }
+            else if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("");
+                return;
+            }
+            else if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("СЛЫШЬ," + textBox1.Text + " Типа закончил");
+                Hide();
+                Form1 form1 = new Form1();
+                form1.ShowDialog();
+                this.Close();
+            }
+            else { MessageBox.Show("Опа-на, ошибочка вышла, исправляй"); }
+            db.closeConnection();
+        }
+    }
+}
